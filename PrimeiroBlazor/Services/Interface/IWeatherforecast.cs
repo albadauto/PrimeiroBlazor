104a981f1fd77070ee1bf5860f3aacce6ec72c4d
José Adauto <60900449+albadauto@users.noreply.github.com>
2023-11-06 19:53:11 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroBlazor/Services/Interface/IWeatherforecast.cs
@@ -0,0 +1,9 @@
+﻿using PrimeiroBlazor.Models;
+
+namespace PrimeiroBlazor.Services.Interface
+{
+    public interface IWeatherforecast
+    {
+        Task<List<WeatherForecast>> GetAll();
+    }
+}
---
+++ PrimeiroBlazor/Services/WeatherForecastService.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using PrimeiroBlazor.Models;
+using PrimeiroBlazor.Services.Interface;
+
+namespace PrimeiroBlazor.Services
+{
+    public class WeatherForecastService : IWeatherforecast
+    {
+        private readonly string APIURL = "https://192.168.18.225:7237/WeatherForecast";
+        
+        public async Task<List<WeatherForecast>> GetAll()
+        {
+            using(var client = new HttpClient())
+            {
+                var result = await client.GetAsync(APIURL);
+                var read = await result.Content.ReadAsStringAsync();
+                var json = JsonConvert.DeserializeObject<List<WeatherForecast>>(read);
+                return json;
+            }
+        }
+    }
+}
